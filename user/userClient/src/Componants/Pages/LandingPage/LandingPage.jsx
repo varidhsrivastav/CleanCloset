fb5529a29144dba88de447d032cebee8ad97b673
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-01-09 22:32:53 +0530

    Updated services we provide

--- user/userClient/src/Componants/Pages/LandingPage/LandingPage.jsx
+++ user/userClient/src/Componants/Pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Intro from './LandingPageComponents/HeroSection/Intro'
 import IntroCard from './LandingPageComponents/IntroCard/IntroCard'
+import Service from './LandingPageComponents/Services/Service'
 
 const LandingPage = () => {
   return (
     <div>
       <Intro/>
-      <IntroCard/>
+      {/* <IntroCard/> */}
+      <Service/>
     </div>
   )
 }
---
+++ user/userClient/src/Componants/Pages/LandingPage/LandingPageComponents/Services/Service.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./Service.css";
+
+const Service = () => {
+  return (
+    <div className="SectionMainContainer">
+      <div className="SectionContainer">
+        <h1>The Problem we want to solve</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error,
+          earum?
+        </p>
+        <div className="ServiceContent">
+          <div className="SectionCards">
+            <div className="ServiceCardBoday">
+              <div className="serviceIcon">
+                <lord-icon
+                  src="https://cdn.lordicon.com/tqywkdcz.json"
+                  trigger="hover"
+                  // style="width:250px;height:250px"
+                ></lord-icon>
+              </div>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quaerat dolor cum, id ea officia illo consectetur perferendis
+                sequi incidunt mollitia.
+              </p>
+            </div>
+            <div className="ServiceCardBoday">
+              <div className="serviceIcon">
+                <lord-icon
+                  src="https://cdn.lordicon.com/tqywkdcz.json"
+                  trigger="hover"
+                  // style="width:250px;height:250px"
+                ></lord-icon>
+              </div>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quaerat dolor cum, id ea officia illo consectetur perferendis
+                sequi incidunt mollitia.
+              </p>
+            </div>
+            <div className="ServiceCardBoday">
+              <div className="serviceIcon">
+                <lord-icon
+                  src="https://cdn.lordicon.com/tqywkdcz.json"
+                  trigger="hover"
+                  // style="width:250px;height:250px"
+                ></lord-icon>
+              </div>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quaerat dolor cum, id ea officia illo consectetur perferendis
+                sequi incidunt mollitia.
+              </p>
+            </div>
+            <div className="ServiceCardBoday">
+              <div className="serviceIcon">
+                <lord-icon
+                  src="https://cdn.lordicon.com/tqywkdcz.json"
+                  trigger="hover"
+                  // style="width:250px;height:250px"
+                ></lord-icon>
+              </div>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quaerat dolor cum, id ea officia illo consectetur perferendis
+                sequi incidunt mollitia.
+              </p>
+            </div>
+          </div>
+          <div className="ServiceSectionImage">ksdjdsk</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
