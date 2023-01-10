6cefffe389603622f7b198a15bd07ce4d1484f04
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-01-11 00:21:55 +0530

    Updated and added making and services

--- user/userClient/src/Componants/Pages/LandingPage/LandingPage.jsx
+++ user/userClient/src/Componants/Pages/LandingPage/LandingPage.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Intro from './LandingPageComponents/HeroSection/Intro'
 import IntroCard from './LandingPageComponents/IntroCard/IntroCard'
+import MakingDiff from './LandingPageComponents/MakingDiff/MakingDiff'
 import Service from './LandingPageComponents/Services/Service'
 
 const LandingPage = () => {
   return (
     <div>
       <Intro/>
-      {/* <IntroCard/> */}
       <Service/>
+      <MakingDiff/>
     </div>
   )
 }
--- user/userClient/src/Componants/Pages/LandingPage/LandingPageComponents/MakingDiff/MakingDiff.jsx
+++ user/userClient/src/Componants/Pages/LandingPage/LandingPageComponents/MakingDiff/MakingDiff.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
-import './Making.css'
+import React from "react";
+import "./Making.css";
+
+import india from './Image/indiapicMaking.png'
 const MakingDiff = () => {
   return (
-    <div className=''>
-
+    <div className="MakingDiffMainContainer">
+      <div className="MakingDiffConatiner">
+        <div className="MakingDiffPhoto">
+          <img src={india} alt="" />
+        </div>
+        <div className="MakingDiffContent">
+          <h1>How we're making a difference</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            dolores sunt recusandae quo corporis quidem nam at quisquam aliquid,
+            itaque voluptatem assumenda, nihil totam consequatur consectetur
+            possimus repudiandae similique incidunt.
+          </p>
+          <div className="MangingDiffInsideContent">
+            <div className="MakingDiffInsideContentBody">
+              <div className="MakingDiffICBodyIcon">
+                <lord-icon
+                  src="https://cdn.lordicon.com/tqywkdcz.json"
+                  trigger="hover"
+                ></lord-icon>
+              </div>
+              <div className="MakingDiffICBodyContent">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+                autem vel illo fugiat nisi aperiam, veritatis velit eum.
+              </div>
+            </div>
+            <div className="MakingDiffInsideContentBody">
+              <div className="MakingDiffICBodyIcon">
+                <lord-icon
+                  src="https://cdn.lordicon.com/tqywkdcz.json"
+                  trigger="hover"
+                ></lord-icon>
+              </div>
+              <div className="MakingDiffICBodyContent">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+                autem vel illo fugiat nisi aperiam, veritatis velit eum.
+              </div>
+            </div>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            ratione.
+          </p>
+          <div className="MakingDiffButton">
+            <button>Donate Now</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MakingDiff
+export default MakingDiff;
