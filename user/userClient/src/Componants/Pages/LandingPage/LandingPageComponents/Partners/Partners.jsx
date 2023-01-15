a1465bfe3219e6a60a66ce6ab3763cbbc64b24ec
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-01-15 17:03:33 +0530

    added partner

--- user/userClient/src/Componants/Pages/LandingPage/LandingPageComponents/Partners/Partners.jsx
+++ user/userClient/src/Componants/Pages/LandingPage/LandingPageComponents/Partners/Partners.jsx
@@ -1,11 +1,80 @@
-import React from 'react'
+import React from "react";
+import "./Partner.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+import "swiper/css";
+import cbf from "./Image/clothsBoxFoundation.png";
+import gf from "./Image/gf.png";
+import logo from "./Image/logo.gif";
+import logo2 from "./Image/logo.png";
+import musk from "./Image/MuskLogo.png";
+import nkf from "./Image/nkf.png";
+import smf from "./Image/smf.webp";
+import udaan from "./Image/udaan-new-logo-big.png";
 
 const Partners = () => {
   return (
-    <div className='ParterMainContainer'>
-      our partner
+    <div className="ParterMainContainer">
+      <h1>Trusted By</h1>
+      <div className="PartnerContainer container">
+        <div className="CompaniesLogo">
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={5}
+            autoplay={{
+              delay: 1500,
+             
+            }}
+            modules={[Autoplay, Pagination, Navigation]}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>
+              <div className="Logo1 logo">
+                <img src={cbf} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="Logo2 logo">
+                <img src={gf} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="Logo3 logo">
+                <img src={logo} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="Logo4 logo">
+                <img src={logo2} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="Logo5 logo">
+                <img src={musk} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="Logo5 logo">
+                <img src={nkf} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="Logo5 logo">
+                <img src={smf} alt="" />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="Logo5 logo">
+                <img src={udaan} alt="" />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Partners
+export default Partners;
--- user/userClient/webpack.config.js
+++ user/userClient/webpack.config.js
@@ -26,10 +26,11 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|webp)$/i,
         use: [
           {
             loader: "file-loader",
+            
           },
         ],
       },
