a206bb958279b7d2f6c9eb0beb6aa180c3b1cc4a
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-01-17 00:45:09 +0530

    Added Footer

--- user/userClient/src/App.jsx
+++ user/userClient/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
+import Footer from "./Componants/Pages/Footer/Footer";
 import LandingPage from "./Componants/Pages/LandingPage/LandingPage";
 import Navbar from "./Componants/Pages/Navbar/Navbar";
 
@@ -10,6 +11,7 @@ const App = () => (
   <div className="">
  <Navbar/>
  <LandingPage/>
+ <Footer/>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
--- user/userClient/src/Componants/Pages/Footer/Footer.jsx
+++ user/userClient/src/Componants/Pages/Footer/Footer.jsx
@@ -1,14 +1,62 @@
-import React from 'react'
-
+import React from "react";
+import "./Footer.css"
 const Footer = () => {
   return (
-    <div className='FooterMainContainer'>
-    <div className="FooterComponants">
-        
+    <div className="FooterMainContainer">
+      <div className="FooterComponants ">
+        <div className="FooterAddNgoSection ">
+        <div className="container FooterAddNgoSection">
+          <div className="AddNgoContent">
+            <h1> Lets Boost Your Business with Digimar Agency </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
+              maiores.
+            </p>
+          </div>
+          <div className="AddNgoButton">
+            <button>add ngo</button>
+          </div>
+        </div>
+        </div>
+        <div className="FooterMainSection">
+        <div className="container FooterMainSection">
+        <div className="FooterLogo">
+            <h1>
+              Clean
+              <br /> Closet
+            </h1>
+            <p>Copyright @ 2023</p>
+          </div>
+          <div className="FooterMenu">
+            <h4>Menu</h4>
+            <div className="FooterMenuLinks">
+            
+            <a href="/">Home</a>
+            <a href="/">Home</a>
+            <a href="/">Home</a>
+            <a href="/">Home</a>
+            </div>
+          </div>
+          <div className="FooterContactUs">
+            <h4>Contact Us</h4>
+            <div className="FooterContactUsLinks">
+              <a href="/">Facebook</a>
+              <a href="/">Facebook</a>
+              <a href="/">Facebook</a>
+            </div>
+          </div>
+          <div className="FooterSocialMedia">
+            <h4>Social Media</h4>
+            <a href="/">icon</a>
+            <a href="/">icon</a>
+            <a href="/">icon</a>
+          </div>
+        </div>
+    
+        </div>
+      </div>
     </div>
-      
-    </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
