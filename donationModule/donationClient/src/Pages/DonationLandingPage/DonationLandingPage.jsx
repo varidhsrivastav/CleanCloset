8a65dfa25166d3efff217c5936715b1afdcf094f
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-01-17 19:15:24 +0530

    Added stats card

--- donationModule/donationClient/src/Pages/DonationLandingPage/DonationLandingPage.jsx
+++ donationModule/donationClient/src/Pages/DonationLandingPage/DonationLandingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DonationHeader from './DonationLandingPageComponants/DonationHeader/DonationHeader'
+import DonationStats from './DonationLandingPageComponants/DonationStats/DonationStats'
 
 const DonationLandingPage = () => {
   return (
     <div>
         <DonationHeader/>
+        <DonationStats/>
     </div>
   )
 }
