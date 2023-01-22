e3b902d410930f6ad2a57bbb65300a9a2296e6fc
Varidh Srivastav <varidhsrivastava19145@gmail.com>
2023-01-22 13:12:39 +0530

    Added form

--- donationModule/donationClient/src/Pages/DonationClientForm/DonationBasicDetailForm/DoationBasicDetailForm.jsx
+++ donationModule/donationClient/src/Pages/DonationClientForm/DonationBasicDetailForm/DoationBasicDetailForm.jsx
@@ -1,9 +1,110 @@
-import React from 'react'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+import logo from "./Image/CleanCloset.png";
+import "./DonationBasicDetail.css";
+import "swiper/css";
+import "swiper/css/pagination";
+// import "swiper/css/navigation";
 
 const DoationBasicDetailForm = () => {
   return (
-    <div>DoationBasicDetailForm</div>
-  )
-}
+    <div className="DoationBasicDetailFormMContainer">
+      <div className="DoationBasicDetailFormContainer">
+        <div className="DoationBasicDetailFormLSide">
+          <div className="DoationBasicDetailFormLSideC">
+            <div className="DoationBasicDetailFormLogo">
+              <img src={logo} alt="" />
+            </div>
+            <div className="DoationBasicDetailFormIntroContent">
+              <h1>Start your journey with us</h1>
+              <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="DoationBasicDetailFormCourosel">
+            <Swiper
+            modules={[Autoplay, Pagination, Navigation]}
+            spaceBetween={50}
+            slidesPerView={1}
+            autoplay={{
+              delay: 2500,
+             
+            }}
+            pagination={{
+          clickable: true,
+        }}
+            
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+          <SwiperSlide><div className="DoationBasicDetailFormCouroselBody">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. loren10 Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, earum.
+                  Iure,sit?
+                </p>
+                <div className="DBDFProfileinfo">
+                  <div className="profilePic">
+                    <img src={logo} alt="" />
+                  </div>
+                  <div className="NameAndNgoName">
+                    <div className="profielName">
+                      <h6>Varidh Srivastav</h6>
+                      <p>CleanCloset</p>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div></SwiperSlide>
+          <SwiperSlide><div className="DoationBasicDetailFormCouroselBody">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. loren10 Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, earum.
+                  Iure,sit?
+                </p>
+                <div className="DBDFProfileinfo">
+                  <div className="profilePic">
+                    <img src={logo} alt="" />
+                  </div>
+                  <div className="NameAndNgoName">
+                    <div className="profielName">
+                      <h6> Mineh H Patel</h6>
+                      <p>CleanCloset</p>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div></SwiperSlide>
+          </Swiper>
+              
+            </div>
+            
+          </div>
+        </div>
+        <div className="DoationBasicDetailFormRSide">
+          <div className="DoationBasicDetailFormRSideBody">
+            <h1>
+              Tell us a little bit more about yourself and we'll get the ball
+              rolling
+            </h1>
+            <div className="DoationBasicDetailFormRSideForm">
+              <form action="">
+                <div className="DBDFinputFields">
+                  <label htmlFor="">Name</label>
+                  <input type="text" placeholder="Your Full Name" />
+                </div>
+                <div className="DBDFinputFields">
+                  <label htmlFor="">Email</label>
+                  <input type="email" placeholder="you@example.com" />
+                </div>
+                <div className="DBDFinputFields">
+                  <label htmlFor="">Phone number</label>
+                  <input type="number" placeholder="+91 909 090 9090" />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default DoationBasicDetailForm
+export default DoationBasicDetailForm;
